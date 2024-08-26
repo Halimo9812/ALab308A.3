7ca9a8240baec3f2876d6212de50fff02d980a82
Halimo9812 <Halimoaali98@gmail.com>
2024-08-26 15:53:59 -0400

    second commit

--- labp.js
+++ labp.js
@@ -1,11 +1,28 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
-   
-  const dbs = {
+async function getUserData(id) {
+    const dataBaseName = await central(id);
+    console.log(dataBaseName)
+
+   //step two users basic info
+   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
+   const basicInfo = await dbs[dataBaseName](id)
+   console.log(basicInfo)
+
+   //step three : access to vault and steal personal data
+   const personalData = await vault(id);
+   console.log(personalData);
+   return {
+    ...basicInfo,
+    ...personalData
+   }
+
 }
+
+const user = await getUserData(6)
+console.log(user);
